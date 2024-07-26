edb754e46bc6829f06f938d7e2a2e47c372e0210
David Liu <davidliu2117@gmail.com>
2024-07-25 23:37:49 -0700

    Added About component to page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import BreakdownSection from "./components/BreakdownSection";
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <BreakdownSection />
+        <About />
       </div>
     </>
   );
