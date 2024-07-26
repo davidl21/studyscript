2eda106526a6c6e51cc83aaafe1e213a2954675b
David Liu <davidliu2117@gmail.com>
2024-07-25 23:26:25 -0700

    added icons to breakdown section

--- src/components/BreakdownSection.jsx
+++ src/components/BreakdownSection.jsx
@@ -19,8 +19,8 @@ const BreakdownSection = () => {
         {features.map((feature, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
             <div className="flex">
-              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
-                Hello
+              <div className="flex mx-6 h-10 w-10 p-2 text-purple-500 justify-center items-center">
+                {feature.icon}
               </div>
               <div>
                 <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
