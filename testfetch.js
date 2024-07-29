93fa5063ec99d79c07fa49e830522216a13cbc43
David Liu <dliu@esri.com>
2024-07-29 11:12:37 -0700

    updated dependencies

--- testfetch.js
+++ testfetch.js
@@ -1,19 +1,6 @@
 import { YoutubeTranscript } from "youtube-transcript";
-import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube";
 
-const loader = YoutubeLoader.createFromUrl(
-  "https://www.youtube.com/watch?v=KSAPc5NwLYU",
-  {
-    language: "en",
-    addVideoInfo: true,
-  }
+const response = await YoutubeTranscript.fetchTranscript(
+  "https://www.youtube.com/watch?v=4bgL-4v3quk"
 );
-
-const docs = await loader.load();
-
-console.log(docs);
-
-// const response = await YoutubeTranscript.fetchTranscript(
-//   "https://www.youtube.com/watch?v=4bgL-4v3quk"
-// );
-// console.log(response);
+console.log(response);
