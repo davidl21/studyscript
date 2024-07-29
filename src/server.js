71e062695b176c2fd89732ac3fef247d07c43a67
David Liu <dliu@esri.com>
2024-07-29 11:12:16 -0700

    connected mongodb cluster

--- src/server.js
+++ src/server.js
@@ -3,8 +3,43 @@ import cors from "cors";
 import needle from "needle";
 import { YoutubeTranscript } from "youtube-transcript";
 import rateLimit from "express-rate-limit";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+// MongoDB Deployment
+const uri = "mongodb+srv://davidl21:ShX2jDrspIYuDMo1@cluster0.4qtdodj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1, 
+    strict: true,
+    depcrecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // connect the client to the server
+    await client.connect();
+
+    // send a ping to confirm connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const app = express();
+
+// Combine text chunks from youtube-transcript api
+const combineText = (req, res, next) => {
+  res.combineText = (transcripts) => {
+    const combinedText = transcripts.map(chunk => chunk.text).join(' ');
+    return combinedText;
+  };
+  next();
+}
+app.use('/get-transcript', combineText);
 
 // rate limiting
 const limiter = rateLimit({
@@ -25,11 +60,17 @@ app.get("/", (req, res) => {
   res.send("Successful response.");
 });
 
-app.get("/transcript", async (req, res) => {
-  const apiRes = await YoutubeTranscript.fetchTranscript(
-    "https://www.youtube.com/watch?v=KSAPc5NwLYU"
-  );
-  res.send(apiRes);
+app.get("/get-transcript", async (req, res) => {
+  try {
+    const apiRes = await YoutubeTranscript.fetchTranscript(
+      "https://www.youtube.com/watch?v=KSAPc5NwLYU"
+    );
+    
+    const combinedText = res.combineText(apiRes);
+    res.json({ combinedText })
+  } catch (error) {
+    res.status(500).send('Error fetching transcript');
+  }
 });
 
 app.listen(3000, () => console.log("App is listening on port 3000."));
