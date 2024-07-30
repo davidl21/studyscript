2689c3893c0d5ff00ff74fb9a9467c181b930182
David Liu <davidliu2117@gmail.com>
2024-07-30 09:54:42 -0700

    login route complete

--- src/server/server.js
+++ src/server/server.js
@@ -130,25 +130,47 @@ app.post("/login", async (req, res) => {
   const myDatabase = client.db("studyscript");
   const myCollection = myDatabase.collection("users");
 
-  // look for user in db
-  try {
-    const findResult = myCollection.find({
-      email: req.query.email,
+  myCollection
+    .findOne({ email: req.query.email })
+    .then((user) => {
+      bcrypt
+        .compare(req.query.password, user.password)
+        .then((passwordCheck) => {
+          if (!passwordCheck) {
+            return res.status(400).send({
+              message: "Passwords do not match.",
+              error,
+            });
+          }
+
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          res.status(200).send({
+            message: "Login successful",
+            email: user.email,
+            token,
+          });
+        })
+        .catch((error) => {
+          res.status(400).send({
+            message: "Passwords do not match",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      res.status(404).send({
+        message: "Email not foud",
+        e,
+      });
     });
-
-    const user_email = findResult;
-  } catch (error) {
-    res.status(404).send({
-      message: "Email not found",
-      error,
-    });
-  }
-
-  // retrieve password from db TODO
-
-  (user_email) => {
-    bcrypt.compare(req.query.password, user_);
-  };
 });
 
 app.post("/get-transcript", async (req, res) => {
