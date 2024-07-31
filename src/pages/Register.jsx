e8f5732de74834e76fa0b7632c9dc506fac01f21
David Liu <dliu@esri.com>
2024-07-31 12:31:00 -0700

    register route complete and connected to frontend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [registered, setRegistered] = useState(false);
+
+  const handleSubmit = (e) => {
+    try {
+      const config = {
+        method: "post",
+        url: "http://localhost:3000/register",
+        params: {
+          user_id: username,
+          email: email,
+          password: password
+        }
+      }
+
+      axios(config);
+      setRegistered(true);
+      console.log("Successfully made API call and registered user.")
+    } catch (error) {
+      console.log("Issue making API call and registering user", error)
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -9,28 +35,31 @@ const Register = () => {
         <h1 className="mt-20 text-4xl sm:text-6xl lg:text-7xl text-center font-thin">
           Join Now!
         </h1>
-        <form className="className=w-[750px] relative">
+        <form onSubmit={(e) => handleSubmit(e)} className="w-[750px] relative">
           <div className="flex flex-col items-center mt-10 relative">
             <input
               type="username"
               placeholder=" Username"
+              onChange={(e) => setUsername(e.target.value)}
               className="w-96 p-2 rounded-full bg-gradient-to-r from-zinc-800 to-zinc-900 border border-neutral-700/80"
             />
             <input
               type="email"
               placeholder=" Email"
+              onChange={(e) => setEmail(e.target.value)}
               className="mt-2 w-96 p-2 rounded-full bg-gradient-to-r from-zinc-800 to-zinc-900 border border-neutral-700/80"
             />
             <input
               type="password"
               placeholder=" Password"
+              onChange={(e) => setPassword(e.target.value)}
               className="mt-2 w-96 p-2 rounded-full bg-gradient-to-r from-zinc-800 to-zinc-900 border border-neutral-700/80"
             />
           </div>
+          <button type="submit" className="mt-10 p-2 rounded-md bg-gradient-to-r from-purple-500 to-purple-800">
+            Register
+          </button>
         </form>
-        <button className="mt-10 p-2 rounded-md bg-gradient-to-r from-purple-500 to-purple-800">
-          Register
-        </button>
       </div>
     </>
   );
