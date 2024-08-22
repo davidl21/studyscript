bc1fdc4f1f1204eb8f6c854acc91a383c3d31223
David Liu <davidliu2117@gmail.com>
2024-08-21 23:55:25 -0700

    fixed navbar home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,33 @@
 import logo from "../assets/studyscriptlogo.png";
 import { navItems } from "../constants";
-import { useRef } from 'react';
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const section1 = useRef(null)
-  const section2 = useRef(null)
-  const section3 = useRef(null)
-
-  const scrollToSection = (elementRef) => {
-    window.scrollTo({
-      top: elementRef.current.offsetTop,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between  items-center">
           <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-1" src={logo} alt='logo'/>
-            <span className="text-xl tracking-tight">StudyScript</span>
+            <img className="h-10 w-10 mr-1" src={logo} alt="logo" />
+            <Link to="/">
+              <span className="text-xl tracking-tight">StudyScript</span>
+            </Link>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
-              <li key={index} className="hover:text-neutral-400 transition-color duration-300 onClick={() => scrollToSection(section1)}">
+              <li
+                key={index}
+                className="hover:text-neutral-400 transition-color duration-300 onClick={() => scrollToSection(section1)}"
+              >
                 <a href={item.href}>{item.label}</a>
               </li>
-            ))} 
+            ))}
           </ul>
         </div>
-      </div>    
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar 
+export default Navbar;
