5ace90a0a17d352df212164997804a6d364277e9
David Liu <davidliu2117@gmail.com>
2024-08-25 01:19:28 -0700

    reduced login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
 
-  const onButtonClick = () => {
-    // update with backend
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("/login", { username });
+
+      if (response.status === 200) {
+        sessionStorage.setItem("user_id", response.data.user_id);
+
+        navigate("/search");
+      } else {
+        console.log("Login failed");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
   };
 
   return (
@@ -17,25 +30,26 @@ const Login = () => {
       <Navbar />
       <div className="flex flex-col items-center mt-6 lg:mt-20-20 text-center">
         <h1 className="mt-20 text-4xl sm:text-6xl lg:text-7xl text-center font-thin">
-          Welcome Back!
+          Login to StudyScript!
         </h1>
         <form className="className=w-[750px] relative">
           <div className="flex flex-col items-center mt-10 relative">
             <input
-              type="email"
-              placeholder=" Email"
+              type="text"
+              id="username"
+              placeholder=" Username"
               className="w-96 p-2 rounded-full bg-gradient-to-r from-zinc-800 to-zinc-900 border border-neutral-700/80"
-            />
-            <input
-              type="password"
-              placeholder=" Password"
-              className="mt-2 w-96 p-2 rounded-full bg-gradient-to-r from-zinc-800 to-zinc-900 border border-neutral-700/80"
+              onChange={(e) => setUsername(e.target.value)}
+              required
             />
           </div>
+          <button
+            type="submit"
+            className="mt-10 p-2 rounded-md bg-gradient-to-r from-purple-500 to-purple-800"
+          >
+            Submit
+          </button>
         </form>
-        <button className="mt-10 p-2 rounded-md bg-gradient-to-r from-purple-500 to-purple-800">
-          Submit
-        </button>
       </div>
     </>
   );
